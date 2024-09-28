036d908f8d76a00167ed9f732cee0b6b53dbb038
badr2e <badreprocontact@gmail.com>
2024-09-28 13:24:27 +0200

    Added instructions

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Projet Flask
+# Système de login avec Flask
+
+## Instructions
+
+1. Cloner le dépôt Git :
+   ```bash
+   git clone https://github.com/badr2e/M1_IntroFlask.git
+   ```
+
+2. Aller dans le dossier cloné et installer les dépendances :
+   ```bash
+   cd M1_IntroFlask
+   pip install -r requirements.txt
+   ```
+
+3. Créer et configurer le fichier .env avec les informations suivantes de votre serveur MySQL
+    ```plaintext
+    MYSQL_HOST=
+    MYSQL_USER=
+    MYSQL_PASSWORD=
+    MYSQL_DB=
+    SECRET_KEY=
+    ```
+
+4. Lancer le fichier
+    ```bash
+    python app.py
+    ```
--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import re
 from dotenv import load_dotenv
 import os
 
+# Charger les variables d'environnement
 load_dotenv()
 
+# Initialisation de l'application Flask et de Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-# Configurations de la base de données
+# Configuration de la base de données MySQL
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
@@ -27,7 +29,13 @@ def home():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM user WHERE user_id = %s', (session['user_id'],))
         user = cursor.fetchone()
-        return render_template('home.html', username=user['user_login'], user_date_new=user['user_date_new'], user_date_login=user['user_date_login'], loggedin=True)
+        return render_template(
+            'home.html', 
+            username=user['user_login'], 
+            user_date_new=user['user_date_new'], 
+            user_date_login=user['user_date_login'], 
+            loggedin=True
+        )
     else:        
         flash("Vous n'êtes pas connecté, veuillez vous connecter.", 'info')
         return render_template('home.html', loggedin=False)
@@ -38,7 +46,7 @@ def logout():
     session.pop('loggedin', None)
     session.pop('user_id', None)
     session.pop('username', None)
-    flash('You have been logged out !', 'success')
+    flash('Vous avez été déconnecté !', 'success')
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -47,7 +55,7 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        # Vérification que les champs sont remplis
+        # Vérification des champs obligatoires
         if not email or not password:
             flash('Tous les champs sont obligatoires !', 'danger')
             return redirect(url_for('login'))
@@ -56,11 +64,10 @@ def login():
         cursor.execute('SELECT * FROM user WHERE user_mail = %s', (email,))
         user = cursor.fetchone()
 
-        # Vérification du mot de passe
+        # Vérification des informations de connexion
         if user and bcrypt.check_password_hash(user['user_password'], password):
             cursor.execute('UPDATE user SET user_date_login = NOW() WHERE user_id = %s', (user['user_id'],))
             mysql.connection.commit()
-
             session['loggedin'] = True
             session['user_id'] = user['user_id']
             session['username'] = user['user_login']
@@ -70,7 +77,6 @@ def login():
 
     return render_template('login.html')
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -79,24 +85,21 @@ def register():
         password = request.form.get('password')
         recheck_password = request.form.get('recheck_password')
 
-        # Vérification que tous les champs sont remplis
+        # Vérification des champs obligatoires
         if not email or not username or not password or not recheck_password:
             flash('Tous les champs sont obligatoires !', 'danger')
             return redirect(url_for('register'))
 
-        # Vérification du format de l'email
+        # Vérification de l'email et des mots de passe
         if not is_valid_email(email):
             flash("L'adresse email n'est pas valide !", 'danger')
             return redirect(url_for('register'))
-
-        # Vérification de la correspondance des mots de passe
         if password != recheck_password:
             flash('Les mots de passe ne correspondent pas !', 'danger')
             return redirect(url_for('register'))
 
         # Hash du mot de passe
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM user WHERE user_mail = %s', (email,))
         account = cursor.fetchone()
@@ -105,26 +108,22 @@ def register():
         if account:
             flash('Un compte existe déjà avec cet email !', 'danger')
         else:
-            # Insertion des données de l'utilisateur dans la base de données
-            cursor.execute('INSERT INTO user (user_login, user_password, user_mail, user_compte_id) VALUES (%s, %s, %s, %s)', (username, hashed_password, email, 0))
+            cursor.execute(
+                'INSERT INTO user (user_login, user_password, user_mail, user_compte_id) VALUES (%s, %s, %s, %s)', 
+                (username, hashed_password, email, 0)
+            )
             mysql.connection.commit()
             flash('Compte créé avec succès!', 'success')
-
-            # Connexion automatique après enregistrement
             session['loggedin'] = True
             session['user_id'] = cursor.lastrowid
             session['username'] = username
-
             return redirect(url_for('home'))
 
     return render_template('register.html')
 
-
 def is_valid_email(email):
-    # Expression régulière pour valider un format d'email
     email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return re.match(email_regex, email)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
